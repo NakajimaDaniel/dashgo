4d9f71812d958105bcc644cbd0ff5898da0b5ff8
Daniel Nakajima <nakajimadaniel96@gmail.com>
2021-04-25 14:30:22 -0300

    header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 
 
-import { Flex, Text, Input, Icon } from '@chakra-ui/react'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
 
-import { RiSearchLine } from 'react-icons/ri'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
  
 export function Header() {
   return (
@@ -16,7 +16,7 @@ export function Header() {
       px="6"
       align="center"
     >
-     <Text
+      <Text
         fontSize="3xl"
         fontWeight="bold"
         letterSpacing="tight"
@@ -52,6 +52,39 @@ export function Header() {
         />
 
         <Icon as={RiSearchLine} fontSize="20"  />
+
+      </Flex>
+
+      <Flex
+        align="center"
+        ml="auto"
+
+      >
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine}  fontSize="20" />
+          <Icon as={RiUserAddLine}  fontSize="20" />
+        </HStack>
+
+        <Flex align="center"> 
+
+          <Box mr="4" textAlign="right">
+            <Text>nome</Text>
+            <Text color="gray.300" fontSize="small" >
+              nome@nome.com
+            </Text>
+          </Box>
+
+          <Avatar size="md" name="nome" />
+
+        </Flex>
 
       </Flex>
 
