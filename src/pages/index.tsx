aed9b95491a7c7c8a815e84ab2823ee7fbfba11b
Daniel Nakajima <nakajimadaniel96@gmail.com>
2021-04-25 13:40:26 -0300

    signin page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,64 @@
-import Head from 'next/head'
+import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react'
 
 
 export default function Home() {
   return (
-    <div>
-      dashgo
-    </div>
+    <Flex 
+      w="100vw" 
+      h="100vh" 
+      align="center" 
+      justify="center"
+    >
+      <Flex 
+        as="form" 
+        w="100%" 
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4" >
+
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input 
+              id="email"
+              name="email" 
+              type="email"
+              focusBorderColor="pink.800"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size="lg"
+            />
+          </FormControl>
+          
+          <FormControl>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              id="password"
+              name="password" 
+              type="password"
+              focusBorderColor="pink.800"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size="lg"
+            />
+          </FormControl>
+          
+        </Stack>
+        
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg" >Entrar</Button>
+
+
+      </Flex>
+    </Flex>
   )
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -18,6 +18,11 @@ export const theme = extendTheme({
     }
   },
 
+  fonts: {
+    heading: 'Roboto',
+    body: 'Roboto',
+  },
+
   styles: {
     global: {
       body: {
