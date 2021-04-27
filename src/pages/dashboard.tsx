524795d7838720a538db1017ff651598bf81e70c
Daniel Nakajima <nakajimadaniel96@gmail.com>
2021-04-27 19:38:10 -0300

    dashboard responsive adjustment

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -86,7 +86,7 @@ export default function Dashboard() {
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
 
           <Box
-            p="8"
+            p={["6", "8"]}
             bg="gray.800"
             borderRadius={8}
             pb="4"
@@ -96,7 +96,7 @@ export default function Dashboard() {
           </Box>
 
           <Box
-            p="8"
+            p={["6", "8"]}
             bg="gray.800"
             borderRadius={8}
             pb="4"
