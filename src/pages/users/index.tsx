5685986c19342967e9cbd92be74d4f80f50c025e
Daniel Nakajima <nakajimadaniel96@gmail.com>
2021-05-07 18:09:15 -0300

    user list

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -21,7 +21,20 @@ export default function UserList() {
     const response = await fetch('http://localhost:3000/api/users')
     const data = await response.json();
 
-    return data;
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      }
+    });
+
+    return users;
   })
 
 
@@ -71,19 +84,25 @@ export default function UserList() {
             </Thead>
 
             <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeigth="bold">Nome</Text>
-                    <Text fontSize="sm" color="gray.300">email@emai.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04/04/21</Td>}
 
-              </Tr>
+              {data.map(user => {
+                return (
+                  <Tr key={user.id}>
+                    <Td px={["4", "4", "6"]}>
+                    <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeigth="bold">{user.name}</Text>
+                        <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>{user.createdAt}</Td>}
+                  </Tr>
+
+                )
+              })}
+
             </Tbody>
 
           </Table>
