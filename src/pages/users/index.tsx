1755b285f0d47984d753727df7a39dbfdebf6599
Daniel Nakajima <nakajimadaniel96@gmail.com>
2021-05-10 20:04:43 -0300

    show userlist on page change

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -14,7 +14,7 @@ export default function UserList() {
 
   const [page, setPage] = useState(1);
 
-  const { data, isLoading, isFetching, error } = useUsers();
+  const { data, isLoading, isFetching, error } = useUsers(page);
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -74,7 +74,7 @@ export default function UserList() {
 
               <Tbody>
 
-                {data.map(user => {
+                {data.users.map(user => {
                   return (
                     <Tr key={user.id}>
                       <Td px={["4", "4", "6"]}>
@@ -97,7 +97,7 @@ export default function UserList() {
             </Table>
 
             <Pagination 
-              totalCountOfRegisters={200}
+              totalCountOfRegisters={data.totalCount}
               currentPage={page}
               onPageChange={setPage}
             />
--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -8,9 +8,19 @@ type User = {
   createdAt: string;
 }
 
+type getUsersResponse = {
+  totalCount: number;
+  users: User[];
+}
 
-export async function getUsers(): Promise<User[]> {
-  const { data } = await api.get('users')
+export async function getUsers(page: number): Promise<getUsersResponse> {
+  const { data, headers } = await api.get('users', {
+    params: {
+      page: page
+    }
+  })
+
+  const totalCount = Number(headers['x-total-count'])
   
   const users = data.users.map(user => {
     return {
@@ -25,12 +35,12 @@ export async function getUsers(): Promise<User[]> {
     }
   });
 
-  return users;
+  return {users, totalCount};
 }
 
 
-export function useUsers() {
-  return useQuery('users', getUsers , {
+export function useUsers(page: number) {
+  return useQuery(['users', page], () => getUsers(page) , {
     staleTime: 1000 * 5
   })
 }
